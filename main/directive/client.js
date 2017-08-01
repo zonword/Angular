60cb92320f82cadb7bab090daf4870cc842ac595
Kevin Grondin <kevingrondin@outlook.com>
2017-08-01 16:07:06 +0400

    Create client.js

---
+++ main/directive/client.js
@@ -0,0 +1,12 @@
+(function() {
+    "use strict";
+    angular.module('myApp', [])
+        .directive("client", Client)
+        
+        function Client() {
+           return{
+              restrict: 'E',
+              templateUrl: 'templates/client.html'
+           }
+        }
+})();
